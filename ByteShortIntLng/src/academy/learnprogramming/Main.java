15dc6e08fdf885c60abe81d506a4d9ee530f8437
Chris Rackey <chris.rackey@gmail.com>
2020-12-19 11:34:40 -0500

    learned ranges/widths of long, short, and byte primitive data types

--- ByteShortIntLng/src/academy/learnprogramming/Main.java
+++ ByteShortIntLng/src/academy/learnprogramming/Main.java
@@ -13,5 +13,25 @@ public class Main {
         System.out.println("Busted Max value: =" + (myMaxIntValue + 1));
         System.out.println("Busted Min value: =" + (myMinIntValue - 1));
         int myMaxIntTest = 2_147_483_647;
+
+        byte myMinByteValue = Byte.MIN_VALUE;
+        byte myMaxByteValue = Byte.MAX_VALUE;
+        System.out.println("Byte min value: " + myMinByteValue);
+        System.out.println("Byte max value: " + myMaxByteValue);
+
+        short myMinShortValue = Short.MIN_VALUE;
+        short myMaxShortValue = Short.MAX_VALUE;
+        System.out.println("Short min value: " + myMinShortValue);
+        System.out.println("Short max value: " + myMaxShortValue);
+
+        long myLongValue = 100L;
+        long myMinlongValue = Long.MIN_VALUE;
+        long myMaxlongValue = Long.MAX_VALUE;
+        System.out.println("long min value: " + myMinlongValue);
+        System.out.println("long max value: " + myMaxlongValue);
+        long bigLongLiteralValue = 2_147_483_647_234L;
+        System.out.println(bigLongLiteralValue);
+
+        short bigShortLiteralValue = 32767;
     }
 }
