b334783e71bcbb8203c89865ceefd46475ac2762
Chris Rackey <chris.rackey@gmail.com>
2020-12-21 17:12:35 -0500

    started intro to java methods

---
+++ JavaMethodsIntro/src/academy/learnprogramming/Main.java
@@ -0,0 +1,59 @@
+package academy.learnprogramming;
+
+public class Main {
+
+    public static void main(String[] args) {
+        boolean gameOver = true;
+        int score = 4000;
+        int levelCompleted = 5;
+        int bonus = 100;
+
+        int highScore = calculateScore(gameOver, score, levelCompleted, bonus);
+        System.out.println("your final score was: " + highScore);
+        int secondScore = 10000;
+        int secondLevelCompleted = 8;
+        int secondBonus = 200;
+        highScore = calculateScore(gameOver, score, levelCompleted, bonus);
+        System.out.println("your final score was: " + highScore);
+
+        int highScorePosition = calculateHighScorePosition(1500);
+        displayHighScorePosition("Tim", highScorePosition);
+
+        highScorePosition = calculateHighScorePosition(900);
+        displayHighScorePosition("Tim", highScorePosition);
+
+        highScorePosition = calculateHighScorePosition(400);
+        displayHighScorePosition("Tim", highScorePosition);
+
+        highScorePosition = calculateHighScorePosition(50);
+        displayHighScorePosition("Tim", highScorePosition);
+
+    }
+
+    public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus){
+
+        if(gameOver){
+            int finalScore = score + (levelCompleted * bonus);
+            finalScore += 2000;
+
+            return finalScore;
+        }else{
+            return -1;
+        }
+    }
+
+    public static void displayHighScorePosition(String name, int position){
+        System.out.println("Player " + name + " managed to make position " + position + " on the high score table");
+    }
+    public static int calculateHighScorePosition(int score){
+        if(score >= 1000){
+            return 1;
+        }else if (score >= 500 && score < 1000) {
+            return 2;
+        }else if (score >= 100 && score < 500){
+            return 3;
+        } else {
+            return 4;
+        }
+    }
+}
