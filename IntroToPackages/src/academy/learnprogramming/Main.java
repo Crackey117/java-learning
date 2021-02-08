0ee722f7d3bb495285f571e41a56a498a199b762
Chris Rackey <chris.rackey@gmail.com>
2021-02-08 16:08:27 -0500

    started learning about using packages in java

---
+++ IntroToPackages/src/academy/learnprogramming/Main.java
@@ -0,0 +1,18 @@
+package academy.learnprogramming;
+
+
+//import javafx.scene.Node;
+
+
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //Node node = null;
+        org.w3c.dom.Node anotherNode = null;
+        MyWindow myWindow = new MyWindow("Complete Java");
+        myWindow.setVisible(true);
+    }
+}
