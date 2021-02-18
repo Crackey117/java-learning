ac85084087993d1c3e629c996914530198a4e9d8
Chris Rackey <chris.rackey@gmail.com>
2021-02-17 20:35:37 -0500

    started learning about Java Maps

---
+++ IntroToJavaCollections/src/academy/learnprogramming/MapProgram.java
@@ -0,0 +1,26 @@
+package academy.learnprogramming;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapProgram {
+    public static void main(String[] args) {
+        Map<String, String> languages = new HashMap<>();
+        languages.put("Java", "a compiled high level, object-oriented, platform independent language");
+        languages.put("Python", "an interpreted, object oriented, high level programming language, with dynamic semantics");
+        languages.put("Algol", "an algorithmic language");
+        System.out.println(languages.put("BASIC", "Beginners All Purpose Symbolic Instruction Code"));
+        languages.put("Lisp", "Therein lies madness");
+        if(languages.containsKey("Java")){
+            System.out.println("Java is already in the map");
+        }else {
+            languages.put("Java", "Coffee is good");
+        }
+        System.out.println(languages.get("Java"));
+        System.out.println(languages.put("Java", "Coffee is good"));
+        System.out.println("================");
+        for(String key: languages.keySet()){
+            System.out.println(key + " : " + languages.get(key));
+        }
+    }
+}
