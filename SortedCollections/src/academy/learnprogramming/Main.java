f320bb29dbaa75d0ea8999356d238645503b9675
Chris Rackey <chris.rackey@gmail.com>
2021-03-02 13:37:41 -0500

    continued learning about collections in java

--- SortedCollections/src/academy/learnprogramming/Main.java
+++ SortedCollections/src/academy/learnprogramming/Main.java
@@ -1,8 +1,28 @@
 package academy.learnprogramming;
 
 public class Main {
-
+    private static  StockList stockList = new StockList();
     public static void main(String[] args) {
-	// write your code here
+	    StockItem temp = new StockItem("Bread", .86, 100);
+	    stockList.addStock(temp);
+
+	    temp = new StockItem("Cake", 1.20, 7);
+        stockList.addStock(temp);
+        temp = new StockItem("Cake", 1.20, 7);
+        stockList.addStock(temp);
+        temp = new StockItem("Car", 70.10, 2);
+        stockList.addStock(temp);
+        temp = new StockItem("Hot sauce", 1.20, 5);
+        stockList.addStock(temp);
+        temp = new StockItem("Phone", 43.23, 1);
+        stockList.addStock(temp);
+        temp = new StockItem("Door", 3.42, 1);
+        stockList.addStock(temp);
+        temp = new StockItem("Juice", 2.12, 2);
+        stockList.addStock(temp);
+        temp = new StockItem("Towel", 1.12, 1);
+        stockList.addStock(temp);
+
+        System.out.println(stockList);
     }
 }
